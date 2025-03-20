2c1334142ae551562b91c281d9397ddf5749e463
redwan-shawkat <redwanshawkat@gmail.com>
2025-03-20 15:49:33 +0600

    red

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -76,6 +76,11 @@ $app->middleware([
     App\Http\Middleware\ExampleMiddleware::class
 ]);
 
+$app->routeMiddleware([
+    'staticToken' => App\Http\Middleware\StaticTokenMiddleware::class,
+]);
+
+
 // $app->routeMiddleware([
 //     'auth' => App\Http\Middleware\Authenticate::class,
 // ]);
